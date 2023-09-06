6717372303a257d8f5ccd6c2f818a0f133b9234b
larcondo <larcondo90@gmail.com>
2023-09-06 18:26:14 -0300

    modifications exercise 2.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import { useState } from 'react'
+
 function App() {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    setPersons([...persons, {name: newName}])
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      { persons.map( person => {
+        return <p key={person.name}>{ person.name }</p>
+      })}
     </div>
   )
 }
